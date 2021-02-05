943e8898f62ecbf04a34b0a88ecc90a7460ba04b
priyanka damodar <priyadamodar12@gmail.com>
2021-02-04 20:33:27 -0500

    so far prompted the length of password and prompted the character types include uppercase and lowercase, also validated the password length.

--- script.js
+++ script.js
@@ -2,11 +2,43 @@
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
+
 function writePassword() {
-    var name;
-    name= prompt("enter your name");
-    console.log(name);
-    console.log("hello , "+ name);
+
+  var passwordCriteria = "Password Length will be ";
+
+  var PasswordLength = prompt("Enter the Length of Password");
+  passwordCriteria = passwordCriteria + PasswordLength;
+
+  if(PasswordLength < 8 || PasswordLength > 128){
+    alert("wrongLength");
+  }
+  var LowercaseNeeded = confirm("Do you want lowercase in password!");
+  
+  if(LowercaseNeeded){
+    passwordCriteria = passwordCriteria + " will contain lowercase ";
+  }
+   else{
+    passwordCriteria = passwordCriteria + " will not contain lowecase ";
+   }
+   alert(passwordCriteria);
+
+   var UppercaseNeeded = confirm("Do you want uppercase in password!");
+   
+   if(UppercaseNeeded){
+    
+    passwordCriteria = passwordCriteria + " and contain uppercase"
+  }
+   else{
+    passwordCriteria = passwordCriteria + " and not contain uppercase"
+   }
+   alert(passwordCriteria);
+
+
+
+
+    
+    
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
